c080d750d05ace5f18775c89f302bc870b99f37c
Samuel Edson <samueledshotmailgm@gmail.com>
2020-02-26 09:29:35 -0300

    Pedro fez para aparecer os alunos cadastrados na tabela da tela professor

---
+++ build/classes/bean/alunoBEAN.java
@@ -0,0 +1,59 @@
+package bean;
+public class alunoBEAN {
+    int matricula;
+    String nome;
+    String email;
+    String telefone;
+    String serie;
+    String escola;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getEscola() {
+        return escola;
+    }
+
+    public void setEscola(String escola) {
+        this.escola = escola;
+    }
+    
+    
+}
---
+++ build/classes/dao/ProfessorDao.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import bean.ProfessorBean;
+import connection.conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ProfessorDao {
+    
+    public void create(ProfessorBean p){ //Função para salvar os dados
+        Connection con = conexao.getConnection(); //Variável que irá receber a conexão
+        PreparedStatement stmt = null; //Variável que irá guardar o comando SQL
+        try {
+            if(verificar(p.getEmail())){
+                JOptionPane.showMessageDialog(null,"Este Email já existe");
+            }else{
+            stmt = con.prepareStatement("INSERT INTO professores(nome,email,telefone,escola_per,disciplina,senha) VALUES (?,?,?,?,?,?)"); //Varável stmt irá receber os comando SQL
+            stmt.setString(1, p.getNome());
+            stmt.setString(2, p.getEmail());
+            stmt.setString(3, p.getTelefone());
+            stmt.setString(4, p.getEscola_pertencente());
+            stmt.setString(5, p.getDisciplina());
+            stmt.setString(6, p.getSenha());
+
+            
+            stmt.executeUpdate();//Executar o comando
+            
+            JOptionPane.showMessageDialog(null,"Salvo com Sucesso"); // Exibir mensagem caso tudo funcione
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Erro ao salvar" + ex);
+        }finally{
+            conexao.closeConnection(con, stmt);
+        }
+    }
+    
+    public void atualizar(ProfessorBean p, String email){
+        Connection con = conexao.getConnection();
+        PreparedStatement stmt = null;
+        
+        try {
+            stmt = con.prepareStatement("update professores set nome = ?, email = ?, telefone = ?,escola_per = ?, disciplina = ?, senha = ? where email = ?");
+            stmt.setString(1, p.getNome());
+            stmt.setString(2, p.getEmail());
+            stmt.setString(3, p.getTelefone());
+            stmt.setString(4, p.getEscola_pertencente());
+            stmt.setString(5, p.getDisciplina());
+            stmt.setString(6, p.getSenha());
+            stmt.setString(7, email);
+            
+            stmt.executeUpdate();//Executar o comando
+            
+            JOptionPane.showMessageDialog(null,"Atualizado com Sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Erro ao Atualizar: "+ex);
+        }finally{
+            conexao.closeConnection(con, stmt);
+        }
+    }
+    
+    public boolean verificar(String email){
+        Connection con = conexao.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = con.prepareStatement("SELECT * FROM professores WHERE email = ?");
+            stmt.setString(1, email);
+            rs = stmt.executeQuery();
+            
+            if(rs.next()){
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERRO: "+ex);
+        }
+        
+        return false;
+    }
+    
+}
