5b1b8c0b44cfaf3bd999c86b0c6da6b6db9d99d3
Samuel Edson <samueledshotmailgm@gmail.com>
2020-03-13 19:57:23 -0300

    Leia - Me modificado (13/03/2020) e icone adicionado

--- src/view/SplashScreen.java
+++ src/view/SplashScreen.java
@@ -27,11 +27,12 @@ public class SplashScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel4 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        barra = new javax.swing.JProgressBar();
         progresso = new javax.swing.JLabel();
+        barra = new javax.swing.JProgressBar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(51, 153, 0));
@@ -39,30 +40,32 @@ public class SplashScreen extends javax.swing.JFrame {
         setResizable(false);
         getContentPane().setLayout(null);
 
-        jLabel4.setFont(new java.awt.Font("Arial", 1, 48)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 102, 0));
-        jLabel4.setText("Online");
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(270, 280, 300, 50);
+        jPanel1.setBackground(new java.awt.Color(51, 153, 0));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel5.setBackground(new java.awt.Color(255, 255, 255));
         jLabel5.setFont(new java.awt.Font("Arial", 1, 48)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
         jLabel5.setText("Professor");
-        getContentPane().add(jLabel5);
-        jLabel5.setBounds(30, 280, 240, 50);
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 140, 240, 50));
 
-        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Arial", 1, 48)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 102, 0));
+        jLabel4.setText("Online");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 140, 300, 50));
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setText("O portal do professor da rede estadual");
-        getContentPane().add(jLabel6);
-        jLabel6.setBounds(110, 330, 310, 20);
-        getContentPane().add(barra);
-        barra.setBounds(90, 370, 270, 14);
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 260, 310, 20));
 
+        progresso.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         progresso.setText("0%");
-        getContentPane().add(progresso);
-        progresso.setBounds(220, 390, 30, 14);
+        jPanel1.add(progresso, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 420, 50, -1));
+        jPanel1.add(barra, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 380, 270, 20));
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 450, 560);
 
         setSize(new java.awt.Dimension(453, 564));
         setLocationRelativeTo(null);
@@ -131,6 +134,7 @@ sc.setVisible(false);
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel progresso;
     // End of variables declaration//GEN-END:variables
 }
